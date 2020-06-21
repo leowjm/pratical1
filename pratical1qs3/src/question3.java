5bc6b6e942bbb0721440c0946723f739182a3e9e
leowjm <66979214+leowjm@users.noreply.github.com>
2020-06-21 13:50:04 +0800

    question3

--- pratical1qs3/src/question3.java
+++ pratical1qs3/src/question3.java
@@ -1,45 +1,42 @@
 import java.util.Scanner;
 
 public class pratical1q3 {
-    public static void main(String [] args){
+    public static void main(String[] args){
+    
+        String creditNum;
+        int sum=0,sum2=0,total=0;
+        
         Scanner scan = new Scanner(System.in);
-        String creditNumber;
-        int sum = 0, sum2 = 0;
-        int total;
         
-        System.out.print("Enter credit card No.: ");
-        creditNumber = scan.nextLine();
+        System.out.print("Enter Creatit No : ");
+        creditNum = scan.nextLine();
         
-        for (int x = 7; x>=0; x-=2){
-            int temp = creditNumber.charAt(x) - '0';
-            sum += temp;
-            System.out.print(temp+"+");
+        for(int x=7;x>=0;x-=2){
+          int temp = creditNum.charAt(x)-'0';
+          sum+=temp;
+            System.out.print(+temp+"+");
+                
         }
-        System.out.print("\b ");       
-        System.out.print("= " + sum + "\n");
+        System.out.print("\b="+sum+".\n");
         
-        for (int x = 6; x>=0; x-=2 ){
-            int temp = creditNumber.charAt(x)-'0';
-            System.out.print(temp * 2 +" " );
-        }
-        
-        System.out.print("\n");
-        for (int x = 6; x>=0; x-=2 ){
-            int temp = creditNumber.charAt(x)-'0';
-            int value = temp * 2;
-            int no1 = value % 10;
-            int no2 = value / 10;
-            
-            sum2 += no1 + no2;
-            System.out.print(no2+"+"+no1+"+");
-          
-        }
-        System.out.print("\b ");
-        System.out.print("=" + sum2);
-        System.out.print("\n");
-        
-        total = sum + sum2;
-        System.out.print(sum + "+" + sum2 + "=" + total + "\n");
-        
+       for(int x=6;x>=0;x-=2){
+        int temp = creditNum.charAt(x)-'0';
+        temp*=2;
+        System.out.print(+temp+" ");
+       }
+       System.out.print("\n");
+       
+       for(int x=6;x>=0;x-=2){
+        int temp= creditNum.charAt(x)-'0';
+        temp*=2;   
+        int no1=temp%10;
+        int no2=temp/10;
+        sum2+=no1+no2; 
+        System.out.print(+no2+"+"+no1+"+");
+         
+       }
+     System.out.print("\b="+sum2+".\n");
+     total=sum+sum2;
+     System.out.print(+sum+"+"+sum2+"="+total+".\n");
     }
 }
